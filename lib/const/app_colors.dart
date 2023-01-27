76f00e3e9f6a89714b96c4c78e60e028530c0300
Natthapol.dev <75296217+Raphdevv@users.noreply.github.com>
2023-01-27 22:36:29 +0700

    test commit

---
+++ lib/const/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color(0xFF1F2630);
+  static const yellowColor = Color(0xFFFCD434);
+  static const greenColor = Color(0xFF2FBE86);
+  static const whiteColor = Color(0xFFFFFFFF);
+
+  static const overlayColor = Color(0xFF454C5E);
+
+  static const primaryTwoColor = Color(0xFF1B2E69);
+}
