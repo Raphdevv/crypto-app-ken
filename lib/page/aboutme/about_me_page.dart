87f246dd7dc4959af796d7a3207d6240fdec5bbb
Natthapol.dev <75296217+Raphdevv@users.noreply.github.com>
2023-02-19 19:18:39 +0700

    add qr code line bot

--- lib/page/aboutme/about_me_page.dart
+++ lib/page/aboutme/about_me_page.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_app/const/app_colors.dart';
 import 'package:crypto_app/widget/template_bg.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -16,12 +17,62 @@ class AboutMe extends StatelessWidget {
           elevation: 0,
         ),
         body: Center(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20.r),
-            child: Image.asset(
-              'assets/images/aboutme.png',
-              alignment: Alignment.center,
-            ),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.r),
+                child: Image.asset(
+                  'assets/images/aboutme.png',
+                  alignment: Alignment.center,
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text(
+                    'กดที่นี่ -->',
+                    style: TextStyle(
+                      color: AppColors.whiteColor,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        barrierDismissible: true,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            backgroundColor: Colors.transparent,
+                            insetPadding: EdgeInsets.zero,
+                            contentPadding: EdgeInsets.zero,
+                            content: InteractiveViewer(
+                              clipBehavior: Clip.none,
+                              child: Image.asset(
+                                'assets/images/qrcode.png',
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Image.asset(
+                      'assets/images/qrcode.png',
+                      scale: 5,
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
